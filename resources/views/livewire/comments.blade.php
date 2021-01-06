8d513028edcdab9147b3029d75bce3c4691aee23
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-06 05:36:14 +0100

    Tailwind CSS

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,3 +1,20 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day --}}
+<div class="flex justify-center">
+    <div class="w-6/12">
+    <h1 class="my-1o text-3xl">Comments</h1>
+    <div class="my-4 flex">
+        <input type="text" name="" id="" placeholder="What's New ?" class="w-full rounded border shadow p-2 mr-2 my-2">
+        <div class="py-2">
+            <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add</button>
+        </div>
+    </div>
+    <div class="rounded border shadow p-3 my-2">
+        <div class="flex justify-start my-2">
+            <p class="font-bold text-lg">Name</p>
+            <p class="mx-3 py-1 text-xs text-gray-500 font-semibold">Time</p>
+        </div>
+        <p class="text-gray-800">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi officia, recusandae dolor inventore optio illum debitis ut qui veritatis maiores nulla eveniet! Provident praesentium mollitia sunt iste saepe illum eius?
+        </p>
+    </div>
+    </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,8 @@
     @livewireStyles
 </head>
 <body>
-    @livewire('counter')
+    {{-- @livewire('counter') --}}
+    @livewire('comments')
     @livewireScripts
 </body>
 </html>
