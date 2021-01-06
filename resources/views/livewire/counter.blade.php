ce107c8342c4da924b1d2973e84e50c15bdce893
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-06 05:07:12 +0100

    Including other components

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day --}}
+<h3>Hello World</h3>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     @livewireStyles
 </head>
 <body>
-
+    @livewire('counter')
     @livewireScripts
 </body>
 </html>
