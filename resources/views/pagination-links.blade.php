b98d8fd09be3a73b2136f773d6a4ed373a93f4a5
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-08 21:33:58 +0100

    Boundaries on links

--- resources/views/pagination-links.blade.php
+++ resources/views/pagination-links.blade.php
@@ -1,8 +1,13 @@
 @if ($paginator->hasPages())
 
 <ul class='flex justify-between'>
-    <li class='w-16 px-2 py-1 text-center rounded border shadow bg-gray-500' wire:click='previousPage'>Prev</li>
-    <li class="w-16 px-2 py-1 text-center rounded border shadow bg-gray-500" wire:click='nextPage'>Next</li>
+    @if (!$paginator->onFirstPage())
+
+    <li class='w-16 px-2 py-1 text-center rounded border cursor-pointer shadow bg-gray-500' wire:click='previousPage'>Prev</li>
+    @endif
+    @if ($paginator->hasMorePages())
+    <li class="w-16 px-2 py-1 text-center rounded border cursor-pointer shadow bg-gray-500" wire:click='nextPage'>Next</li>
+    @endif
 </ul>
 
 @endif
