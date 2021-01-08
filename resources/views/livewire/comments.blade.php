381b85baf94016302cc5bd8d3432816012f44868
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-08 20:48:04 +0100

    Seed data

--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -18,7 +18,7 @@
 
         <input wire:model='newComment' type="text" name="" id="" placeholder="What's New ?" class="w-full rounded border shadow p-2 mr-2 my-2">
         <div class="py-2">
-            <button type='submit' class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add</button>
+            <button type='submit' class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add ➕</button>
         </div>
     </form>
     @foreach ($comments as $comment)
