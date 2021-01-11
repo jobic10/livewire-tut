8b455ee6135345bac0e2d685d1338d5777cf0107
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-11 16:29:49 +0100

    Passing data

--- app/Http/Livewire/Tickets.php
+++ app/Http/Livewire/Tickets.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use App\Models\SupportTicket;
 class Tickets extends Component
 {
     public function render()
     {
-        return view('livewire.tickets');
+        return view('livewire.tickets',
+    [
+        'tickets' => SupportTicket::all()
+    ]);
     }
 }
--- resources/views/livewire/tickets.blade.php
+++ resources/views/livewire/tickets.blade.php
@@ -1,3 +1,12 @@
 <div>
-<h2>Tickets</h2>
+<h1 class="text-3x">Support Tickets</h1>
+
+@foreach ($tickets as $ticket)
+
+<div class="rounded border shadow p-3 my-2">
+
+    <p class="text-gray-800 break-words">{{ $ticket->question }}</p>
+
+</div>
+@endforeach
 </div>
