7d5beef73716dd20cdfbc93f9c1320b8ffbb7d4f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-13 17:36:40 +0100

    Added Turbo to App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     @livewireStyles
     @livewireScripts
-
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body class="flex justify-center">
     <div class="flex w-full justify-center">
